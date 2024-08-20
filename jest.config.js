beabf501f30fdcdf3dfaf4465781e34958acaaf4
Esra Sarıgül <esra.sarigul14@gmail.com>
2024-08-20 15:52:13 +0300

    deleted test files jest-redux integration

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    preset: "jest-expo",
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transformIgnorePatterns: [
-      "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)",
-    ],
-    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  };
-  
+  preset: "jest-expo",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|react-redux|redux|@reduxjs/toolkit)",
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+};
